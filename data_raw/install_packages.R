9e8c2a59b6898fe85811468b0a0b4f237c5cd932
Markus Kainu <muuankarski@kapsi.fi>
2021-03-08 18:04:28 +0200

    "here","writexl","zoo"

--- data_raw/install_packages.R
+++ data_raw/install_packages.R
@@ -1,3 +1,3 @@
 # CRAN
-libs_cran <- c("shiny","shinycssloaders","gt","dplyr","readxl","geofi","ggplot2","tidyr","sf","ggrepel","glue","knitr","patchwork","stringr","xtable","extrafont","forcats","viridis","purrr","janitor","remotes","geofi")
+libs_cran <- c("here","writexl","zoo","shiny","shinycssloaders","gt","dplyr","readxl","geofi","ggplot2","tidyr","sf","ggrepel","glue","knitr","patchwork","stringr","xtable","extrafont","forcats","viridis","purrr","janitor","remotes","geofi")
 inst <- match(libs_cran, .packages(all=TRUE)); install.packages(libs_cran[which(is.na(inst))])
