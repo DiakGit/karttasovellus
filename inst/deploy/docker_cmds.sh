4147aca45383cc85c57061c850530221339a5907
Markus Kainu <markus.kainu@kapsi.fi>
2022-03-18 15:47:58 +0200

    dcrk

--- inst/deploy/docker_cmds.sh
+++ inst/deploy/docker_cmds.sh
@@ -1,5 +1,5 @@
 IMAGENAME=karttasovellus
 docker build -t $IMAGENAME .
 # testaa ensin interaktiivisesti R:ssä esim. ajamalla < R -e "shiny::runApp()" >
-docker run -ti --rm reseptilaakesovellus bash
+docker run -ti --rm karttasovellus bash
 docker run --env-file .Renviron $IMAGENAME
